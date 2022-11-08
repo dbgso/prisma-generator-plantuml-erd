17a5b91d87fcc042b4031722e7d023ec8bdcbe15
dbgso <s092018@gmail.com>
2022-11-08 21:32:16 +0900

    fix: npm audit

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -1,5 +1,4 @@
 import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper';
-import { logger } from '@prisma/sdk';
 import { GENERATOR_NAME } from './constants';
 import { PlantUmlErdGenerator } from './plantuml-erd';
 
@@ -7,7 +6,6 @@ const { version } = require('../package.json');
 
 generatorHandler({
   onManifest() {
-    logger.info(`${GENERATOR_NAME}:Registered`);
     return {
       version,
       defaultOutput: '../generated',
--- packages/generator/src/helpers/index.ts
+++ packages/generator/src/helpers/index.ts
@@ -1,4 +1,3 @@
-import { DMMF } from '@prisma/generator-helper';
 import { getDMMF, getSchemaSync } from '@prisma/sdk';
 
 export const getDMMFFromFile = (filepath: string) => {
